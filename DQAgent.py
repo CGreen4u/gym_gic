107d219d3b51d7600871d9581d97520828d06475
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2020-08-20 12:38:14 -0600

    Add files via upload

--- DQAgent.py
+++ DQAgent.py
@@ -20,7 +20,11 @@ env = gym.make('GuessingGame-v0') # initialise environment
 
 #we need to change the four from hard code later
 #this is just the way we plan to move.
-state_size = 4 #env.observation_space.n
+'''Data to include  would say: AL, AU, SymH, Bz, and Kp might be a good initial, include local time-magnetic local time, lat/long, in correlation to the earth and sun'''
+
+
+state_size = 1 #env.observation_space.n #we changed this to one because ther is only one input for state as of now
+# in order to get the four the states need to be gic, and three other variables proabaly
 print(state_size)
 
 action_size = env.action_space.shape[0]
@@ -28,7 +32,7 @@ print(action_size)
 
 batch_size = 32 #running thing concurently really matters if running gpu
 
-n_episodes = 1001 # n games we want agent to play (default 1001) the more games we play the more data we get for training
+n_episodes = 2001 # n games we want agent to play (default 1001) the more games we play the more data we get for training
 #each episode we learn little bit and we pass it on to train with.
 
 output_dir = 'model_output/guessinggame/'
@@ -53,15 +57,15 @@ class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000) # double-ended queue; acts like list, but elements can be added/removed from either end 
+        self.memory = deque(maxlen=20000) # double-ended queue; acts like list, but elements can be added/removed from either end 
         #memory saved from episodes. saves us from going over the full episode and looks at samples randomly thought game rather than contious
         #sampling will also add diversity so it doesnt always start with going up or down. deque is a list that cuts down last 2000 memories
         self.gamma = 0.95 # decay or discount rate: enables agent to take into account future actions in addition to the immediate ones, but discounted at this rate
         self.epsilon = 1.0 # exploration rate: how much to act randomly; more initially than later due to epsilon decay
         self.epsilon_decay = 0.995 # decrease number of random explorations as the agent's performance (hopefully) improves over time
         self.epsilon_min = 0.01 # minimum amount of random exploration permitted
-        self.learning_rate = 0.001 # rate at which NN adjusts models parameters via SGD to reduce cost 
-        self.model = self._build_model() # private method 
+        self.learning_rate = 1e-3 # rate at which NN adjusts models parameters via SGD to reduce cost 
+        self.model = self._build_model() # private method below (basically a regular net)
     
     def _build_model(self):
         # neural net to approximate Q-value function:
@@ -111,27 +115,69 @@ class DQNAgent:
         self.model.save_weights(name)
 
 
-'''initalize agen'''
+'''initalize agent'''
 agent = DQNAgent(state_size, action_size) # initialise agent
 '''Interact with environment'''
 done = False #default is game has not ended.
 for e in range(n_episodes): # iterate over new episodes of the game
     state = env.reset() # reset state at start of each new episode of the game
-    print(state)
+    #print(state)
     #state = np.reshape(state, [0, state_size]) #just changing from a row to a column, simple transpose
     for time in range(5000):  # time represents a frame of the game; goal is to stay within the 1% range. once its done it can go on for only 5000 steps e.g., 500 or 5000 timesteps
 #         env.render() #moving my animation to render
-        action = agent.act(state) # action is 0 - 4 (move up, down, equal or no guess); decide on one or other here
-        next_state, reward, done, _ = env.step(action) #env.step(np.array([guess])) # agent interacts with env, gets feedback; 4 state data points, e.g., pole angle, cart position        
-        reward = reward if not done else -10 # reward +1 for each additional frame with pole upright        
+        action = agent.act(np.array(state)) # action is 0 - 4 (move up, down, equal or no guess); decide on one or other here
+        next_state, reward, done, _ = env.step(np.array(action)) #env.step(np.array([guess])) # agent interacts with env, gets feedback; 4 state data points, e.g., high or low - observation - later the variables and  done, reward        
+        print(reward)
+        #reward = reward if not done else -10 # reward +1 for each additional frame with pole upright        
         #next_state = np.reshape(next_state, [1, state_size])
         agent.remember(state, action, reward, next_state, done) # remember the previous timestep's state, actions, reward, etc.        
         state = next_state # set "current state" for upcoming iteration to the current next state        
         if done: # episode ends if agent is outside 1% or we reach timestep 5000
-            print("episode: {}/{}, score: {}, e: {:.2}" # print the episode's score and agent's epsilon 
+            print("episode: {}/{}, number of guesses-score: {}, e: {:.2}" # print the episode's score and agent's epsilon 
                   .format(e, n_episodes, time, agent.epsilon)) #if agent isnt working well epsoline is where we should look
             break # exit loop
     if len(agent.memory) > batch_size:
         agent.replay(batch_size) # train the agent by replaying the experiences of the episode
     if e % 50 == 0:
         agent.save(output_dir + "weights_" + '{:04d}'.format(e) + ".hdf5") #saving every 50 episode. we can decide which model weight to hold onto
+
+
+    # state = env.reset()
+
+    # # applying dichotomy # we constantly cut the number in half until we get close
+    # def dichotomize(upper, lower):
+    #     return (upper + lower) / 2.0
+
+    # upper_bound = 15
+    # lower_bound = -15
+
+    # done = False
+
+
+    # # lst = [-1,5,3,4,7,10,9]
+    # # for x in lst:
+    # target = env.env.number
+    # while not done:
+    #     guess = dichotomize(upper_bound, lower_bound)
+    #     #print("[{}, {}, {}]".format(lower_bound, guess, upper_bound))
+
+    #     observation, reward, done, info = env.step(np.array([guess]))
+
+    #     if observation == 1:
+    #         # Guess is lower than the target
+    #         lower_bound = guess
+    #     elif observation == 2:
+    #         # Guess is equal to the target
+    #         print("You Got it")
+    #     elif observation == 3:
+    #         # Guess is higher than the target
+    #         upper_bound = guess
+    #     else:
+    #         raise("Problem")
+
+
+    # print("Guess: {} | Target: {}".format(guess, target))
+
+    # dr = pd.read_csv(r'C:\Users\cgree\Documents\Astra\Space_weather5_22\weakley_all\guess.csv') # open guess csv
+    # dr = dr.append({'guess': guess}, ignore_index=True) # add value
+    # dr.to_csv(r'C:\Users\cgree\Documents\Astra\Space_weather5_22\weakley_all\guess.csv',index=False) # save and close updated csv
--- guessing_game.py
+++ guessing_game.py
@@ -6,6 +6,9 @@ from gym.utils import seeding
 import os
 os.chdir(r"C:\Users\cgree\Documents\Astra\Space_weather5_22")
 from number_tracker import number_tracking
+#https://docs.ray.io/en/latest/rllib-env.html # advanced multi agent
+#https://stable-baselines.readthedocs.io/en/master/modules/a2c.html #alternative to gym
+
 
 class GuessingGame(gym.Env):
     """Number guessing game
@@ -16,14 +19,21 @@ class GuessingGame(gym.Env):
     After each step the agent is provided with one of four possible observations
     which indicate where the guess is in relation to the randomly chosen number
 
+    We will update the observation to be the 4 varaiables as well.
     0 - No guess yet submitted (only after reset)
     1 - Guess is lower than the target
     2 - Guess is equal to the target
     3 - Guess is higher than the target
 
-    The rewards are:
-    0 if the agent's guess is outside of 1% of the target
-    1 if the agent's guess is inside 1% of the target
+    #The rewards are:
+    #0 if the agent's guess is outside of 1% of the target
+    #1 if the agent's guess is inside 1% of the target
+
+    The rewards is calculated as:
+    (min(action, self.number) + self.range) / (max(action, self.number) + self.range)
+    Ideally an agent will be able to recognise the 'scent' of a higher reward and
+    increase the rate in which is guesses in that direction until the reward reaches
+    its maximum
 
     The episode terminates after the agent guesses within 1% of the target or
     200 steps have been taken
@@ -42,44 +52,48 @@ class GuessingGame(gym.Env):
     
     
     def __init__(self):
-        self.range = 3  # Randomly selected number is within +/- this value
-        self.bounds = 10
-
+        self.range = 4  # Randomly selected number is within +/- this value
+        self.bounds = 4
         self.action_space = spaces.Box(low=np.array([-self.bounds]), high=np.array([self.bounds]),
-                                       dtype=np.float32)
+                                       dtype=np.float64)
         self.observation_space = spaces.Discrete(4)
 
         self.number = 0
         self.guess_count = 0
-        self.guess_max = 8
-        self.observation = 0
+        self.guess_max = 20
+        self.observation = 0 #always starts at zero because it hasn't seen anything
 
         self.seed()
         self.reset()
         #print("break-this is in source code its the initial reset being called so the number comes alive at this point")
-
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def step(self, action):
-        assert self.action_space.contains(action)
-
+        #print(action[0])
+        #assert self.action_space.contains(action[0]), "%r (%s) invalid" % (action[0], type(action[0])) #simply verifying that the information being introduced is actually in our action space -4 through 4 
+        #we need to add the observation of the random variables here. just so it can view it on observation
+        #we want it to view the last 10 states in observations instead of 0 after first 
+        #last_position, last_change_in_velocity = self.state
+        reward = 0
+        
         if action < self.number:
             self.observation = 1
 
         elif action == self.number:
             self.observation = 2
-
+            
         elif action > self.number:
             self.observation = 3
 
-        reward = 0
         done = False
 
-        if (self.number - self.range * 0.0001) < action < (self.number + self.range * 0.0001):
-            reward = 1
+        if (self.number - self.range * 0.005) < action < (self.number + self.range * 0.005):
+            reward += ((min(action, self.number) + self.bounds) / (max(action, self.number) + self.bounds)) ** 2
             done = True
+            if reward == 1:
+                reward += 10
 
         self.guess_count += 1
         if self.guess_count >= self.guess_max:
